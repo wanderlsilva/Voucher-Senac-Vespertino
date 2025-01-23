6ac96d5f645bd78e5b8c8fb7ba8d2f35875edb15
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-01-22 21:04:47 -0400

    Manipulação de arquivos
    Introdução a manipulação de arquivos em python.

---
+++ UC III - Desenvolver algoritmos/introdução manipulação de arquivos/arq.py
@@ -0,0 +1,7 @@
+arquivo = open('arquivos/palavras.txt', 'w')
+
+arquivo.write('Abacaxi\n')
+arquivo.write('Morango\n')
+arquivo.write('Manga')
+
+arquivo.close()
---
+++ UC III - Desenvolver algoritmos/introdução manipulação de arquivos/arq1.py
@@ -0,0 +1,10 @@
+#arquivo = open('arquivos/palavras.txt', 'r')
+#for linha in arquivo:
+#    print(linha)
+
+#arquivo = open('arquivos/palavras.txt', 'r')
+#linha = arquivo.readline()
+#print(linha)
+
+import random
+print(random.randrange(0,90))
