af11f105c1d27da4594fab1f23ed2e65e4984dca
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-02-24 14:56:12 -0400

    Update

--- UC V - Desenvolver aplicações desktop/poo/conta.py
+++ UC V - Desenvolver aplicações desktop/poo/conta.py
@@ -1,27 +1,48 @@
+import datetime
+
+class Historico:
+    def __init__(self):
+        self.data_abertura = datetime.datetime.today()
+        self.transacoes = []
+
+    def imprime(self):
+        print("Data Abertura: {}".format(self.data_abertura))
+        print("Transsações: ")
+        for t in self.transacoes:
+            print("-", t)
+
+class Cliente:
+    def __init__(self, nome, sobrenome, cpf):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
+
 class Conta:
-    def __init__(self, numero, titular, saldo, limite=1000.0):
+    def __init__(self, numero, cliente, saldo, limite=1000.0):
         print("Inicializando uma conta")
         self.numero = numero
-        self.titular = titular
+        self.titular = cliente
         self.saldo = saldo
         self.limite = limite
+        self.historico = Historico()
 
     def deposita(self, valor):
         self.saldo += valor
-        print("Depósito Realizado!!!")
-
+        self.historico.transacoes.append("Depósito de {}".format(valor))
+                                         
     def saca(self, valor):
         if (self.saldo < valor):
             print("Saldo INSUFICIENTE!!!")
             return False
         else:
             self.saldo -= valor
-            print("Saque Realizado com Sucesso!!!")
+            self.historico.transacoes.append("Saque de {}".format(valor))
             return True
 
     def extrato(self):
         print("Emitindo Extrado na Tela")
         print("numero: {} \nsaldo: {}".format(self.numero, self.saldo))
+        self.historico.transacoes.append("Tirou extrato - saldo de {}".format(self.saldo))
 
     def transfere(self, destino, valor):
         retirou = self.saca(valor)
@@ -29,4 +50,5 @@ class Conta:
             return False
         else:
             destino.deposita(valor)
+            self.historico.transacoes.append("Transferencia de {} para conta {}".format(valor, destino.numero))
             return True
--- UC V - Desenvolver aplicações desktop/poo/teste_conta.py
+++ UC V - Desenvolver aplicações desktop/poo/teste_conta.py
@@ -1,25 +1,7 @@
 from conta import Conta
+from conta import Cliente
 
-c1 = Conta('123-9', 'Wander', 100.0)
-c1.deposita(100.0)
-print("Titular : ", c1.titular)
-print("Saldo : ", c1.saldo)
-print("Limite : ", c1.limite)
-
-c2 = Conta('442-8', 'Maria', 200.0)
-c2.deposita(200.0)
-print("Titular : ", c2.titular)
-print("Saldo : ", c2.saldo)
-print("Limite : ", c2.limite)
-
-c3 = Conta('352-7', 'Carla', 300.0)
-c3.deposita(300.0)
-print("Titular : ", c3.titular)
-print("Saldo : ", c3.saldo)
-print("Limite : ", c3.limite)
-
-c1.transfere(c3, 50.0)
-print("Titular", c1.titular)
-print("Saldo", c1.saldo)
-print("Titular", c3.titular)
-print("Saldo", c3.saldo)
+cliente_1 = Cliente("Wander", "Luiz", "111222333-44")
+conta_1 = Conta("123-4", cliente_1, 300.0)
+print("TItular", conta_1.titular.nome)
+print("Saldo", conta_1.saldo)
