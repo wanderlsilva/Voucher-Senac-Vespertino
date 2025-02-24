3b602bf74bbe0692bcc7cfe5884f1d70f980a766
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-02-24 15:59:00 -0400

    Update

--- UC V - Desenvolver aplicações desktop/poo/teste_conta.py
+++ UC V - Desenvolver aplicações desktop/poo/teste_conta.py
@@ -1,7 +1,17 @@
-from conta import Conta
-from conta import Cliente
+from conta import *
 
-cliente_1 = Cliente("Wander", "Luiz", "111222333-44")
-conta_1 = Conta("123-4", cliente_1, 300.0)
-print("TItular", conta_1.titular.nome)
-print("Saldo", conta_1.saldo)
+cliente_1 = Cliente('Maria', 'Silva', '111222333-44')
+cliente_2 = Cliente("José", "Da Silva", "444555666-77")
+conta_1 = Conta("123-4", cliente_1, 500.0)
+conta_2 = Conta("234-5", cliente_2, 200.0)
+
+conta_1.deposita(100.0)
+conta_1.saca(50.0)
+conta_1.transfere(conta_2, 200.0)
+conta_1.extrato
+
+conta_1.historico.imprime()
+conta_2.historico.imprime()
+
+
+
