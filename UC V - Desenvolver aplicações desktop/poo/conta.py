451133e9dd46f770ffa2b3f5731f0028424f3037
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-02-24 14:18:34 -0400

    Update

--- UC V - Desenvolver aplicações desktop/poo/conta.py
+++ UC V - Desenvolver aplicações desktop/poo/conta.py
@@ -1,5 +1,5 @@
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, titular, saldo, limite=1000.0):
         print("Inicializando uma conta")
         self.numero = numero
         self.titular = titular
--- UC V - Desenvolver aplicações desktop/poo/teste_conta.py
+++ UC V - Desenvolver aplicações desktop/poo/teste_conta.py
@@ -1,22 +1,25 @@
 from conta import Conta
 
-c1 = Conta('123-9', 'Wander', 100.0, 4000.0)
+c1 = Conta('123-9', 'Wander', 100.0)
 c1.deposita(100.0)
 print("Titular : ", c1.titular)
 print("Saldo : ", c1.saldo)
+print("Limite : ", c1.limite)
 
-c2 = Conta('442-8', 'Maria', 200.0, 4000.0)
+c2 = Conta('442-8', 'Maria', 200.0)
 c2.deposita(200.0)
 print("Titular : ", c2.titular)
 print("Saldo : ", c2.saldo)
+print("Limite : ", c2.limite)
 
-c3 = Conta('352-7', 'Carla', 300.0, 4000.0)
+c3 = Conta('352-7', 'Carla', 300.0)
 c3.deposita(300.0)
 print("Titular : ", c3.titular)
 print("Saldo : ", c3.saldo)
+print("Limite : ", c3.limite)
 
 c1.transfere(c3, 50.0)
 print("Titular", c1.titular)
 print("Saldo", c1.saldo)
-
+print("Titular", c3.titular)
 print("Saldo", c3.saldo)
